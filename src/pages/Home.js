8b0c7eb8cf18fa11648be1d94d3c11afc8fc34ca
SamirMehdibeyli <106684028+SamirMehdibeyli@users.noreply.github.com>
2023-06-19 12:58:43 +0400

    3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { Link } from "react-router-dom";
+import Banner from "../images/banner.jpeg";
+import '../styles/HomeStyles.css'
 
 function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className="home" style={{ backgroundImage: `url(${Banner})`}}>
+        <div className="headerContainer">
+          <h1>Food Webiste</h1>
+          <p>Best Food In Azerbaijan</p>
+          <Link to="./menu">
+            <button>Order Now</button>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
